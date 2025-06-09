44f6cb253806e9a6c2ad81b8d9b246e36c1577cd
farazahmed16821 <farazahmed16821@gmail.com>
2025-06-09 01:04:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// script.js
 
+document.addEventListener("DOMContentLoaded", function () {
+  const whatsappButtons = document.querySelectorAll(".product-whatsapp-button");
+  whatsappButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      alert("واٹس ایپ پر رابطہ کرنے کے لیے ری ڈائریکٹ ہو رہے ہیں۔");
+    });
+  });
+});
