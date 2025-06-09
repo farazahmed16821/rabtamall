3f858a1482d92a209c4d74f52bd15da3131329a9
farazahmed16821 <farazahmed16821@gmail.com>
2025-06-09 01:56:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ const products = [
   { title: "Naya Product", image: "images/newimage.jpg" },
   { title: "Doosra Product", image: "images/secondimage.jpg" }
 ];
+const products = [
+  // پہلے سے موجود پروڈکٹس...
+  { title: "Stylish Shirt", image: "images/shirt1.jpg" },
+  { title: "Elegant Watch", image: "images/watch1.jpg" },
+  { title: "Casual Shoes", image: "images/shoes1.jpg" },
+  { title: "Sunglasses", image: "images/sunglasses1.jpg" },
+  { title: "Leather Wallet", image: "images/wallet1.jpg" },
+
+  // آپ کے لیے نئے واچ باکسز:
+  { title: "Classic Watch", image: "images/watch2.jpg" },
+  { title: "Sporty Watch", image: "images/watch3.jpg" }
+];
